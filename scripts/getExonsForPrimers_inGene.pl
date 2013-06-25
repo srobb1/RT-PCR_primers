d2e844acca4af3f52501865249e70f80f5516292
Sofia Robb <sofia2341@gmail.com>
2013-06-25 10:33:15 -0700

    added runnig of primer3 and parsing output

--- scripts/getExonsForPrimers_inGene.pl
+++ scripts/getExonsForPrimers_inGene.pl
@@ -3,7 +3,6 @@ use warnings;
 use strict;
 use Bio::DB::SeqFeature::Store;
 use Data::Dumper;
-
 my $sqlite = shift;
 my $gene_file = shift;
 die "Please provide a SQLite datafile of a seqfeature db" if !defined $sqlite;
@@ -19,13 +18,18 @@ my $db_obj = Bio::DB::SeqFeature::Store->new(
   -dsn     => $sqlite
 );
 
+my $scripts = '/rhome/robb/src/RT-PCR_primers/scripts';
+my $primer3_path = '/rhome/robb/src/primer3-2.3.5/src';
+open P3IN , ">$gene_file.P3IN" or die "Can't open $gene_file.P3IN for writing\n";
+
+
 my @features;
 open IN, $gene_file , or die "Can't open $gene_file\n";
 while (my $gene = <IN>){
   chomp $gene;
   push @features , $db_obj->get_features_by_name($gene);
 }
-open OUTEXONS, ">exons_for_primers.txt" or die "Can't open transcript_exons_info.txt";
+#open OUTEXONS, ">exons_for_primers.txt" or die "Can't open transcript_exons_info.txt";
 foreach my $feature ( @features ) {
   my $f_name  = $feature->name;
   my $f_start = $feature->start;
@@ -50,23 +54,26 @@ foreach my $feature ( @features ) {
     ## get seq of exon
     my $e_seq =$db_obj->fetch_sequence(-seq_id=>$ref,-start=>$e_start,-end=>$e_end);
     push @exons , $e_seq;
-    #push @overlaps , $overlaps[-1] + (length $e_seq);
+    if (@overlaps > 0){
+      push @overlaps , $overlaps[-1] + (length $e_seq);
+    }else {#first exon
+      push  @overlaps , (length $e_seq);
+    }
   }
+  ##get rid of last element
+  pop @overlaps;
   print "$f_name has only one exon\n" if @exons == 0;
-  my $seq = join ('-',@exons);
-  #print OUTEXONS "$f_name\t$ref\t$start,$end\t".join(';',@exons)."\n";
-  print ">$f_name\n$seq\n";
+  ## for some reason primer3 is not liking the '-'
+  #my $seq = join ('-',@exons);
+  my $seq = join ('',@exons);
+  #print ">$f_name\n$seq\n";
 
 
-}
-__END__
 ## add write of primer3 input file
-my $overlaps = join (" ",@overlaps);
-  #print P3IN "
-  print  "
-SEQUENCE_ID=$f_name
-SEQUENCE=$seq
-SEQUENCE_OVERLAP_JUNCTION=$overlaps
+  print P3IN "SEQUENCE_ID=$f_name
+SEQUENCE_TEMPLATE=$seq
+PRIMER_PRODUCT_SIZE_RANGE=100-150
+SEQUENCE_OVERLAP_JUNCTION_LIST=@overlaps
 PRIMER_OPT_SIZE=22
 PRIMER_MIN_SIZE=18
 PRIMER_MAX_SIZE=25
@@ -77,9 +84,11 @@ PRIMER_OPT_GC=50
 PRIMER_MAX_GC=80
 PRIMER_MIN_GC=40
 PRIMER_GC_CLAMP=1
+PRIMER_THERMODYNAMIC_PARAMETERS_PATH=$primer3_path/primer3_config/
 =\n";
-}
 
-#`primer3_core < $cwd/primers/$fasta.primer3in > $cwd/primers/$fasta.primer3out`;
+}                                                                           
+`$primer3_path/primer3_core < $gene_file.P3IN > $gene_file.primer3out`;
+print `$scripts/parsePrimer3.pl $gene_file.primer3out`;
 
-                                                                           
+
